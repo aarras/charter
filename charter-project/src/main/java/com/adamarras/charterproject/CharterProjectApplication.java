be74f015b058e643b07178beaa28897e9d88d42a
Adam Arras <adamarras@gmail.com>
2021-06-16 20:10:26 -0500

    initial commit

---
+++ charter-project/src/main/java/com/adamarras/charterproject/CharterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.adamarras.charterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharterProjectApplication.class, args);
+	}
+
+}
---
+++ charter-project/src/test/java/com/adamarras/charterproject/CharterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamarras.charterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
